b71e0d614ff3dfa56dcc61f441990fa97cfd52ec
Bhogeswararao <92287678+Bhogeswararao@users.noreply.github.com>
2021-10-25 19:27:55 +0530

    Add files via upload

---
+++ DEL.py
@@ -0,0 +1,16 @@
+x=3
+y=5
+z=x+y    #addition
+a = x-y  #subtraction
+b = x%y  #modulus
+c = x/y  #division
+d = x//y #floor division
+e = x*y  #multiplication
+f = x**y #exponent
+print(z)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
