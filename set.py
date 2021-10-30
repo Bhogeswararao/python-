92718116b009b067c4c20d8f88643731710ed07a
Bhogeswararao <92287678+Bhogeswararao@users.noreply.github.com>
2021-10-30 17:29:59 +0530

    Update set.py

--- set.py
+++ set.py
@@ -1,2 +1,14 @@
 girl_friends={'ramya','kotalaxmi','sangeetha','varalaxmi','sravani','ramya'}
 print(girl_friends)
+#to know the data type
+print(type(girl_friends))
+#to clear the element in set
+girl_friends.remove('sangeetha')
+print(girl_friends)
+#to remove the last element in set
+girl_friends.pop()
+print(girl_friends)
+#to add element to the list
+girl_friends.add('RAMYA BHOGESH')
+print(girl_friends)
+#to clear the set
